e46d2c01ee73448aac4f775cbdba3fa60fb41684
Kenshin Abe <abe.kenshin@gmail.com>
2017-11-23 13:48:16 +0900

    Delete unnecessay eval

--- src/board/eval.rs
+++ src/board/eval.rs
@@ -48,15 +48,6 @@ pub fn eval(ref state: &State) -> i32 {
         hand_weight += (state.hand[0].get_num(n) as i32) * WEIGHT[kind_to_piece(n, false) as usize]; // white
     }
     let mut weight = board_weight * 9 / 10 + hand_weight;
-    if state.board[7][4] == 6 {
-        weight += 10;
-    }
-    if state.board[8][4] == 6 {
-        weight += 20;
-    }
-    if state.board[4][4] == 1 {
-        weight += 5;
-    }
     if state.color {
         weight
     } else {
