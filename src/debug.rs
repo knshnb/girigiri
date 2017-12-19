ef6402a4f36a742ced427e362e7a492b9d11479a
Kenshin Abe <abe.kenshin@gmail.com>
2017-12-19 16:59:49 +0900

    Convert input to byte code

--- src/debug.rs
+++ src/debug.rs
@@ -18,16 +18,17 @@ fn main() {
         let mut input = String::new();
         io::stdin().read_line(&mut input)
             .expect("failed to read line");
-        if input.len() == 2 {
+        let bytes = input.as_bytes();
+        if bytes.len() == 2 {
             // undo
             state.print_move(&mv);
             state.undo_move(&mv);
         } else {
-            let to_j: i8 = 9 - (input.chars().nth(2).unwrap().to_digit(10).unwrap() as i8);
-            let to_i: i8 = input.chars().nth(3).unwrap() as i8 - 'a' as i8;
-            if input.chars().nth(1) == Some('*') {
+            let to_j = '9' as i8 - bytes[2] as i8;
+            let to_i = bytes[3] as i8 - 'a' as i8;
+            if bytes[1] == '*' as u8 {
                 // drop
-                let kind = match input.chars().nth(0).unwrap() {
+                let kind = match bytes[0] as char {
                     'K' => 7,
                     'G' => 6,
                     'R' => 5,
@@ -40,12 +41,12 @@ fn main() {
                 };
                 mv = board::move_encode::Move::drop_encode(kind, to_i, to_j);
             } else {
-                let from_j: i8 = 9 - (input.chars().nth(0).unwrap().to_digit(10).unwrap() as i8);
-                let from_i: i8 = input.chars().nth(1).unwrap() as i8 - 'a' as i8;
-                if input.len() == 6 {
+                let from_j = '9' as i8 - bytes[0] as i8;
+                let from_i = bytes[1] as i8 - 'a' as i8;
+                if bytes.len() == 6 {
                     // promote
                     mv = board::move_encode::Move::promote_encode(from_i, from_j, to_i, to_j);
-                } else if input.len() == 5 {
+                } else if bytes.len() == 5 {
                     // normal
                     mv = board::move_encode::Move::normal_encode(from_i, from_j, to_i, to_j);
                 }
