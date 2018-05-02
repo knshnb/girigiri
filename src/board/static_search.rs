96e35dab8bb4c9b6d1e537cac9b217f473070a4b
Kenshin Abe <abe.kenshin@gmail.com>
2018-05-03 05:49:47 +0900

    Fix static search

--- src/board/static_search.rs
+++ src/board/static_search.rs
@@ -1,5 +1,5 @@
 use std::i32;
-use std::cmp::max;
+use std::cmp;
 use board::state::*;
 use board::eval::*;
 
@@ -11,7 +11,7 @@ fn sub_static_search(ref mut state: &mut State, to_i: i8, to_j: i8) -> i32 {
         if mv.to_i() == to_i && mv.to_j() == to_j {
             is_finish = false;
             state.apply_move(&mv);
-            best_value = max(best_value, -sub_static_search(state, to_i, to_j));
+            best_value = cmp::max(best_value, -sub_static_search(state, to_i, to_j));
             state.undo_move(&mv);
         }
     }
@@ -23,14 +23,14 @@ fn sub_static_search(ref mut state: &mut State, to_i: i8, to_j: i8) -> i32 {
 }
 
 pub fn static_search(ref mut state: &mut State) -> i32 {
-    let mut best_value = eval(&state);
+    let mut best_value = -i32::max_value();
     let moves = state.legal_move_no_drop();
     for mv in moves {
         if state.is_capture(&mv) || state.is_pawn_promote(&mv) {
             state.apply_move(&mv);
-            best_value = max(best_value, -sub_static_search(state, mv.to_i(), mv.to_j()));
+            best_value = cmp::max(best_value, -sub_static_search(state, mv.to_i(), mv.to_j()));
             state.undo_move(&mv);
         }
     }
-    best_value
+    cmp::min(eval(&state), -best_value)
 }
