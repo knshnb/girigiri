ceca3bfa416713132c4cea3295c2cdbce79740ee
Kenshin Abe <abe.kenshin@gmail.com>
2018-05-03 05:02:56 +0900

    Fix static search

--- src/board/static_search.rs
+++ src/board/static_search.rs
@@ -23,20 +23,14 @@ fn sub_static_search(ref mut state: &mut State, to_i: i8, to_j: i8) -> i32 {
 }
 
 pub fn static_search(ref mut state: &mut State) -> i32 {
-    let mut best_value = -i32::max_value();
-    let mut is_finish = true;
+    let mut best_value = eval(&state);
     let moves = state.legal_move_no_drop();
     for mv in moves {
         if state.is_capture(&mv) || state.is_pawn_promote(&mv) {
-            is_finish = false;
             state.apply_move(&mv);
             best_value = max(best_value, -sub_static_search(state, mv.to_i(), mv.to_j()));
             state.undo_move(&mv);
         }
     }
-    if is_finish {
-        eval(&state)
-    } else {
-        best_value
-    }
+    best_value
 }
