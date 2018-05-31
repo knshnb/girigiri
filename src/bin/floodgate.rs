3eb198ef1cafcee4867a1da2c385a870cf6d6bde
Kenshin Abe <abe.kenshin@gmail.com>
2018-05-31 18:39:03 +0900

    Delete warnings

--- src/bin/floodgate.rs
+++ src/bin/floodgate.rs
@@ -2,7 +2,6 @@ extern crate lazy_static;
 extern crate girigiri;
 
 use girigiri::csa::player::*;
-use std::{thread, time};
 
 const USERNAME: &str = "girigiri";
 const PASSWORD: &str = "floodgate-300-10F,hoge";
