b09ed580c874ae2771d8c649a9e00ccf4e9b3cd0
Kenshin Abe <abe.kenshin@gmail.com>
2018-06-08 18:06:52 +0900

    Fix static search

--- src/board/static_search.rs
+++ src/board/static_search.rs
@@ -6,21 +6,20 @@ use board::position::*;
 
 fn sub_static_search(ref mut state: &mut State, to: Position) -> i32 {
     let mut best_value = -i32::max_value();
-    let mut is_finish = true;
     let moves = state.legal_move_no_drop();
     for mv in moves {
         if mv.to_pos() == to {
-            is_finish = false;
             state.apply_move(&mv);
+            if state.opponent_king_is_capturable() {
+                // 王手無視
+                state.undo_move(&mv);
+                continue;
+            }
             best_value = cmp::max(best_value, -sub_static_search(state, to));
             state.undo_move(&mv);
         }
     }
-    if is_finish {
-        eval(&state)
-    } else {
-        best_value
-    }
+    cmp::max(best_value, eval(&state))
 }
 
 pub fn static_search(ref mut state: &mut State) -> i32 {
@@ -29,6 +28,11 @@ pub fn static_search(ref mut state: &mut State) -> i32 {
     for mv in moves {
         if state.is_capture(&mv) || state.is_pawn_promote(&mv) {
             state.apply_move(&mv);
+            if state.opponent_king_is_capturable() {
+                // 王手無視
+                state.undo_move(&mv);
+                continue;
+            }
             best_value = cmp::max(best_value, -sub_static_search(state, mv.to_pos()));
             state.undo_move(&mv);
         }
